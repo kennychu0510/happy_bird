20c3f08aa54b5a57ec02302a20b6abf809f4948a
Kenny Chu <kennychu@asl.com.hk>
2023-01-20 17:29:50 +0800

    added bird class

---
+++ src/bird.ts
@@ -0,0 +1,28 @@
+import { CONSTANTS } from "./constant"
+
+export class Bird {
+  x = 20
+  height = 20
+  width = 20
+  y = CONSTANTS.FRAME_HEIGHT / 2 - this.height / 2
+  ctx: CanvasRenderingContext2D;
+  velocity = CONSTANTS.GRAVITY
+
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx
+  }
+  draw() {
+    this.ctx.fillStyle = 'rgb(0,204,204)'
+    this.ctx.fillRect(this.x, this.y, this.width, this.height)
+    this.update();
+  }
+
+  update() {
+    this.y += this.velocity / 200
+    this.velocity += CONSTANTS.GRAVITY
+  }
+
+  jump() {
+    
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Bird } from './bird';
 import { CONSTANTS } from './constant';
 const { FRAME_WIDTH, FRAME_HEIGHT } = CONSTANTS;
 
@@ -8,9 +9,11 @@ const canvas = selectElement('canvas') as HTMLCanvasElement;
 
 const ctx = canvas.getContext('2d')!;
 const OBSTACLES: Obstacle[] = [new Obstacle(ctx, 0), new Obstacle(ctx, 150)];
+const BIRD = new Bird(ctx)
 function draw() {
   ctx.clearRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
   OBSTACLES.forEach((obstacle) => obstacle.draw());
+  BIRD.draw();
   requestAnimationFrame(draw);
 }
 
